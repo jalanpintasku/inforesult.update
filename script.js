19652ea33b609008d7a4bb7bdb5c779aebaf0024
GERBANG HITAM <161411170+jalanpintasku@users.noreply.github.com>
2024-04-26 09:43:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('NOMOR IMEI DEVICE ANDA SUDAH DIREKAM OTOMATIS OLEH SISTEM UNTUK MENGHINDARI PENYALAHGUNAAN')
+var yakin = confirm("Apakah kamu yakin ingin melanjutkan proses? (SYARAT DAN KETENTUAN YANG BERLAKU TIDAK DAPAT DI UBAH DENGAN ALASAN APAPUN) Tetap ingin melanjutkan ?");
+var nama = prompt("Siapa nama kamu?", "");
+        document.write("<p>Hello "+ nama +"</p>Buka kunci untuk mendapatkan seluruh info ");
+        
